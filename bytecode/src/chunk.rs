083547ec9abd5367005ffa9cf2db7d2cb8951fd6
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-24 15:25:14 -0300

    feat(bytecode): Add default implementation and methods to Chunk struct

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -1,7 +1,27 @@
 use crate::{opcode::OpCode, value::Value};
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
+#[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
 pub struct Chunk {
-    pub code: Vec<OpCode>,
-    pub constants: Vec<Value>,
+    code: Vec<OpCode>,
+
+    constants: Vec<Value>,
+}
+
+impl Chunk {
+    pub fn write(&mut self, opcode: OpCode, line: usize) {
+        self.code.push(opcode);
+    }
+
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
+    }
+
+    pub fn get_code(&self, index: usize) -> Option<OpCode> {
+        self.code.get(index).copied()
+    }
+
+    pub fn get_constant(&self, index: usize) -> Option<Value> {
+        self.constants.get(index).copied()
+    }
 }
