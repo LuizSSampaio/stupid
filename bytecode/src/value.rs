d5f094e164643b072693740f2119558cc58c4bfe
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-24 15:15:56 -0300

    feat(bytecode): Add Display trait implementation for custom Value enum

--- bytecode/src/value.rs
+++ bytecode/src/value.rs
@@ -1,6 +1,18 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub enum Value {
     Number(f64),
     Bool(bool),
     Nil,
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Number(n) => write!(f, "{}", n),
+            Value::Bool(b) => write!(f, "{}", b),
+            Value::Nil => write!(f, "nil"),
+        }
+    }
 }
