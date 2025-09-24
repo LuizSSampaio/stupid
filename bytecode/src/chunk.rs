8d88c7b82583509ff27f3ec238a04e7e14c605b6
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-24 15:08:00 -0300

    feat(bytecode): Add partial ordering to Chunk and OpCode structs

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -1,5 +1,6 @@
 use crate::value::Value;
 
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Chunk {
     pub code: Vec<u8>,
     pub constants: Vec<Value>,
--- bytecode/src/value.rs
+++ bytecode/src/value.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub enum Value {
     Number(f64),
     Bool(bool),
