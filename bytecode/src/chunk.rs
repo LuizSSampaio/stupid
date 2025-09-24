c6736a27550a6a94a15b0137aae7487c823a1ada
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-24 15:25:32 -0300

    feat(bytecode): Add line tracking to bytecode chunk

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -3,6 +3,7 @@ use crate::{opcode::OpCode, value::Value};
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
 pub struct Chunk {
     code: Vec<OpCode>,
+    lines: Vec<usize>,
 
     constants: Vec<Value>,
 }
@@ -10,6 +11,7 @@ pub struct Chunk {
 impl Chunk {
     pub fn write(&mut self, opcode: OpCode, line: usize) {
         self.code.push(opcode);
+        self.lines.push(line);
     }
 
     pub fn add_constant(&mut self, value: Value) -> usize {
@@ -19,6 +21,10 @@ impl Chunk {
 
     pub fn get_code(&self, index: usize) -> Option<OpCode> {
         self.code.get(index).copied()
+    }
+
+    pub fn get_line(&self, index: usize) -> Option<usize> {
+        self.lines.get(index).copied()
     }
 
     pub fn get_constant(&self, index: usize) -> Option<Value> {
