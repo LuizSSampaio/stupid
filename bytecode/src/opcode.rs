8f99995b6d9395bfe87946eedb3069caacf2bfd1
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-26 15:14:56 -0300

    refactor(bytecode): split OpCode enum making unary be a separated enum

--- bytecode/src/opcode.rs
+++ bytecode/src/opcode.rs
@@ -1,6 +1,17 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum OpCode {
     Constant(usize),
-    Negate,
+    Unary(Unary),
     Return,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Unary {
+    Negate,
+}
+
+impl From<Unary> for OpCode {
+    fn from(value: Unary) -> Self {
+        Self::Unary(value)
+    }
 }
