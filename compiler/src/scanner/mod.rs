a6120cd571300c616ea7878ad3c47859a7504562
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-09-30 14:46:33 -0300

    feat(compiler): add scan_token method to Scanner

--- compiler/src/scanner/mod.rs
+++ compiler/src/scanner/mod.rs
@@ -1,5 +1,6 @@
-use crate::scanner::reader::Reader;
 use thiserror::Error;
+
+use crate::scanner::{reader::Reader, token::Token};
 
 mod reader;
 mod token;
@@ -14,6 +15,25 @@ impl Scanner {
         Self {
             reader: Reader::new(source),
         }
+    }
+
+    fn scan_token(&mut self) -> Result<Token, ScanError> {
+        self.reader.start_to_current();
+
+        if self.reader.is_at_end() {
+            return Ok(Token {
+                token_type: token::TokenType::Eof,
+                lexeme: "".to_string(),
+                row: self.reader.row(),
+                column: self.reader.column(),
+            });
+        }
+
+        Err(ScanError::UnexpectedCharacter(
+            self.reader.peek(),
+            self.reader.row(),
+            self.reader.column(),
+        ))
     }
 }
 
