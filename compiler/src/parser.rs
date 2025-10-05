0eb87e8dc41429ddae5359a24da810c09377aecf
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-05 19:11:20 -0300

    feat(compiler): Add scanner field to Parser struct

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -1,15 +1,18 @@
 use crate::scanner::{Scanner, token::Token};
 
 pub struct Parser {
+    scanner: Scanner,
+
     previous: Token,
     current: Token,
 }
 
 impl Parser {
-    pub fn new(scanner: &mut Scanner) -> Self {
+    pub fn new(mut scanner: Scanner) -> Self {
         let first_token = scanner.scan_token().unwrap();
 
         Self {
+            scanner,
             previous: first_token.clone(),
             current: first_token,
         }
