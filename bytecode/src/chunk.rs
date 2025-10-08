7a166c2df128c81354ba902081ba550234fa0d11
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-08 18:00:28 -0300

    feat(bytecode): Change OpCode to u8 in Chunk structure

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -1,8 +1,11 @@
-use crate::{opcode::OpCode, value::Value};
+use crate::{
+    opcode::{OpCode, OpCodeError},
+    value::Value,
+};
 
 #[derive(Debug, Default, Clone, PartialEq, PartialOrd)]
 pub struct Chunk {
-    code: Vec<OpCode>,
+    code: Vec<u8>,
     lines: Vec<usize>,
 
     constants: Vec<Value>,
@@ -10,7 +13,7 @@ pub struct Chunk {
 
 impl Chunk {
     pub fn write(&mut self, opcode: OpCode, line: usize) {
-        self.code.push(opcode);
+        self.code.push(opcode as u8);
         self.lines.push(line);
     }
 
@@ -19,8 +22,8 @@ impl Chunk {
         self.constants.len() - 1
     }
 
-    pub fn get_code(&self, index: usize) -> Option<OpCode> {
-        self.code.get(index).copied()
+    pub fn get_code(&self, index: usize) -> Option<Result<OpCode, OpCodeError>> {
+        self.code.get(index).copied().map(|val| val.try_into())
     }
 
     pub fn get_line(&self, index: usize) -> Option<usize> {
